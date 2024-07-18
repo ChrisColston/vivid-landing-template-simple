805a43155b55ff07afa602a13549d9f04b77e59d
ChrisColston <projects@chriscolston.co.uk>
2024-07-18 10:04:00 +0100

    background image push

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import Image from "next/image";
-
+import { FeatureIcons } from "../svg/FeatureIcons";
 import { ScatteredSpheres } from "../svg/ScatteredSpheres";
 import MacBarSrc from "../public/images/mac-bar.png";
 
